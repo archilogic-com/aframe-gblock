40b353e2bb97048f06432be0788db3dc04b75ad7
Tomas Polach <tomas@polach.ch>
2017-11-08 07:17:28 +0000

    updates download URLs path

--- server/api-methods.js
+++ server/api-methods.js
@@ -146,7 +146,7 @@ exports.getGltfUrl = function getGltfUrlFromGoogleBlocksSite (req, res) {
     url: url
   }, function (error, response, body) {
 
-    var path = 'https:\\/\\/vr\\.google\\.com\\/downloads\\/'
+    var path = 'https:\\/\\/poly\\.google\\.com\\/downloads\\/'
 
     var isRemixable = body.indexOf('Not remixable') === -1
     var objUrl =  new RegExp(`(${path}${id}\\/[-_a-zA-Z0-9]*\\/${id}_obj\\.zip)`).exec(body)
